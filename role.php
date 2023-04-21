7334683ca53ceb38ab1f44187b30d3cf587c50af
AbdouVak <abdourahman.vakouiev@gmail.com>
2023-04-21 15:59:54 +0200

    ajout: fonction addfilm et listeIncarnerRole

--- role.php
+++ role.php
@@ -1,13 +1,28 @@
 <?PHP
 class Role{
     private string $_nom;
+    private array $_acteurs;
 
     public function __construct(string $nom){
         $this->_nom = $nom;
+        $this->_acteurs = [];
     }
 
     public function getNom(){
         return $this->_nom;
+    }
+
+    public function addFilms($acteur){
+        $this->_acteurs[] = $acteur;
+    }
+
+    public function listeRoleIncarner(){
+        $result ="";
+        echo  "Le genre  ".$this->getNom()." est associé ";
+        foreach($this->_acteurs as $acteurs){
+            $result .= $acteurs->getNom()." ".$acteurs->getNom().", ";
+        }
+        return $result."<br>";
     }
 }
 ?>
