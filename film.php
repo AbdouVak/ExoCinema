f8a006e8adf99a569d6b78207f8b4e04f560bda5
AbdouVak <abdourahman.vakouiev@gmail.com>
2023-04-21 14:51:21 +0200

    ajout des getter et setter

--- film.php
+++ film.php
@@ -5,7 +5,7 @@ class Film{
     private string $_titre;
     private Realisateur $_realisateur;
     private Genre $_genre;    
-    private string $_dateSortie;
+    private DateTime $_dateSortie;
     private int $_duree;
     private array $_casting;
 
@@ -13,9 +13,48 @@ class Film{
         $this->_titre = $titre;
         $this->_realisateur = $realisateur;
         $this->_genre = $genre;        
-        $this->_dateSortie = $dateSortie;
+        $this->_dateSortie = new DateTime($dateSortie) ;
         $this->_duree = $duree;
         $this->_casting = [];
+    }
+
+    public function getTitre() :string{
+        return $this->_titre;
+    }    
+    public function setTitre($Titre){
+        $this->_titre = $Titre;
+    }
+    
+    public function getRealisateur():Realisateur{
+        return $this->_realisateur;
+    }    
+    public function setRealisateur($realisateur){
+        $this->_realisateur = $realisateur;
+    }
+
+    public function getGenre():Genre{
+        return $this->_genre;
+    }    
+    public function setGenre($genre) {
+        $this->_genre = $genre;
+    }
+
+    public function getDateSortie() :DateTime{
+        return $this->_dateSortie;
+    }    
+    public function setDateSortie($dateSortie){
+        $this->_dateSortie = $dateSortie;
+    }
+
+    public function getDuree() :int{
+        return $this->_duree;
+    }    
+    public function setDuree($duree){
+        $this->_duree = $duree;
+    }
+
+    public function addCasting(Casting $casting){
+        $this->_casting[] = $casting;
     }
 
     public function afficherFilm(){
@@ -24,10 +63,6 @@ class Film{
                  "Genre: ".$this->_genre."<br>".
                  "Date Sortie: ".$this->_dateSortie."<br>".
                  "Durée: ".$this->_duree." min<br>";
-    }
-
-    public function addCasting(Casting $casting){
-        $this->_casting[] = $casting;
     }
 
     public function afficherCasting(){
